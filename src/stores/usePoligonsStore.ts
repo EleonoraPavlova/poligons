2642b97ee25c4f17eb5e11bc65d051cb2ef331b0
eleonora <nebrudko18@gmail.com>
2025-07-27 15:31:23 +0300

    added toast/fixed loaded

--- src/stores/usePoligonsStore.ts
+++ src/stores/usePoligonsStore.ts
@@ -2,15 +2,22 @@ import { ref, type Ref } from 'vue'
 import { defineStore } from 'pinia'
 
 import { getList, type Polygon } from '@/api/poligons'
+import { useToastStore } from '@/stores/useToastStore'
 
 export const usePoligonsStore = defineStore('poligons', () => {
   const isLoaded = ref(false)
   const poligons: Ref<Polygon[]> = ref([])
+  const toast = useToastStore()
 
   const loadPoligons = async () => {
     if (isLoaded.value) return
-    poligons.value = await getList()
-    isLoaded.value = true
+    try {
+      poligons.value = await getList()
+      isLoaded.value = true
+    } catch (e) {
+      console.error('Error loading polygons:', e)
+      toast.showToast('Error loading polygons')
+    }
   }
 
   return {
--- src/stores/useVillagesStore.ts
+++ src/stores/useVillagesStore.ts
@@ -4,18 +4,24 @@ import { defineStore } from 'pinia'
 import { getList } from '@/api/poligons'
 import { getVillagesByCoords } from '@/api/nominatimApi'
 import type { NominatimPlace } from '@/api/types/nominatim'
+import { useToastStore } from '@/stores/useToastStore.ts'
 
 export const useVillagesStore = defineStore('villages', () => {
   const villages: Ref<NominatimPlace[]> = ref([])
   const isLoaded = ref(false)
+  const toast = useToastStore()
 
   const loadVillages = async () => {
     if (isLoaded.value) return
-    const poligons = await getList()
-
-    const village = await getVillagesByCoords(poligons)
-    villages.value = village
-    isLoaded.value = true
+    try {
+      const poligons = await getList()
+      const village = await getVillagesByCoords(poligons)
+      villages.value = village
+      isLoaded.value = true
+    } catch (error) {
+      console.error('Error loading villages:', error)
+      toast.showToast('Error loading villages')
+    }
   }
 
   return {
