7c5908bdb3031ae0a1cc484ac1700290027e9138
eleonora <nebrudko18@gmail.com>
2025-07-28 09:31:54 +0300

    added proxy/test-server

--- README.md
+++ README.md
@@ -8,13 +8,13 @@ This is a Single Page Application built with Vue 3 that visualizes and interacts
  - Pinia for state management and caching API responses
  - vue-router for routing
  - Leaflet + vue-leaflet for interactive map rendering
- - Turf.js for calculating polygon centers/
+ - Turf.js for calculating polygon centers
  - Nominatim API integration for geocoding and fetching village details
  - Responsive layout with a sidebar for search and village list/details
  - Map auto-focus to show all polygons or a selected village polygon
  - Loading indicators during API calls
  - Error handling with user notifications
- - Caching to minimize redundant API requests/
+ - Caching to minimize redundant API requests
  - Adaptive design optimized for desktops, tablets, and mobile devices
 
 ## Architecture
--- src/api/nominatimApi.ts
+++ src/api/nominatimApi.ts
@@ -3,29 +3,30 @@ import { type Coordinates, type Polygon } from '@/api/poligons'
 import type { NominatimPlace, NominatimPlaceWithGeojson } from './types/nominatim'
 import getCenter from '@/utils/getCenter'
 import pLimit from 'p-limit'
+import { fetchDataFromExternalApi } from '@/api/externalApi.ts'
 
 // in order to speed up the development process, we can mock the response in development mode
 // this way we can avoid hitting the API too hard and can test the application without waiting for the API response
 // in production mode, we will use the real API
+export const BASE_URL = import.meta.env.VITE_NOMINATIM_API
+
 const getVillagesByCoords = async (
   poligons: Polygon[],
   onVillageLoaded?: (village: NominatimPlace) => void,
 ): Promise<NominatimPlace[]> => {
   let villages: NominatimPlace[] = []
+  const errors = []
   if (import.meta.env.MODE === 'production') {
     const limit = pLimit(2)
     await Promise.all(
       poligons.map((polygon) =>
         limit(async () => {
-          try {
-            const village = await getVillageByCoords(getCenter(polygon.polygon))
-            villages.push(village)
-            if (onVillageLoaded) {
-              onVillageLoaded(village)
-            }
-          } catch (e) {
-            console.error('error loading:', e)
-          }
+          const village = await getVillageByCoords(getCenter(polygon.polygon))
+          villages.push(village)
+          onVillageLoaded?.(village)
+        }).catch ((e) => {
+            console.error('error loading village:', e)
+            errors.push({ polygon, error: e })
         }),
       ),
     )
@@ -39,28 +40,15 @@ const getVillagesByCoords = async (
 }
 
 const getVillageByCoords = async (coords: Coordinates): Promise<NominatimPlace> => {
-  const resp = await fetch(
-    `https://nominatim.openstreetmap.org/reverse?lat=${coords.lat}&lon=${coords.lng}&format=json&zoom=10&addressdetails=1`,
-    {
-      headers: {
-        'User-Agent': 'my-test-task', // Важливо!
-      },
-    },
-  )
-  return await resp.json()
+const path = `/reverse?lat=${coords.lat}&lon=${coords.lng}&format=json&zoom=10&addressdetails=1`
+  return await fetchDataFromExternalApi(path)
 }
 
 const getVillageDetails = async (placeId: string): Promise<NominatimPlaceWithGeojson> => {
-  const resp = await fetch(
-    `https://nominatim.openstreetmap.org/lookup?osm_ids=R${placeId}&polygon_geojson=1&format=json`,
-    {
-      headers: {
-        'User-Agent': 'my-test-task', // Важливо!
-      },
-    },
-  )
-  const data: NominatimPlaceWithGeojson[] = await resp.json()
-  return data && data[0]
+const path = `/lookup?osm_ids=R${placeId}&polygon_geojson=1&format=json`
+ const resp: NominatimPlaceWithGeojson[]  =  await fetchDataFromExternalApi(path)
+  return  resp[0]
 }
 
 export { getVillagesByCoords, getVillageDetails }
+//http://localhost:3000/api/proxy?url=https://nominatim.openstreetmap.org/reverse?lat=${coords.lat}&lon=${coords.lng}&format=json&zoom=10&addressdetails=1
