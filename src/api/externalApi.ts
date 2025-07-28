5f91ce669fc74c7b2d92c5c264a680cfe5643d9e
eleonora <nebrudko18@gmail.com>
2025-07-28 15:44:51 +0300

    added localStorage

--- src/api/externalApi.ts
+++ src/api/externalApi.ts
@@ -1,8 +1,17 @@
 import { BASE_URL } from '@/api/nominatimApi.ts'
+import { createStorageCache } from '@/utils/createStorageCache.ts'
+
+const cache = createStorageCache('nominatim', {
+  ttlMs: 60 * 60 * 1000, // 1 час
+})
 
 const VERCEL_PROXY_BASE_URL = '/api/proxy'
 
 export async function fetchDataFromExternalApi(externalResourcePath: string) {
+  const cacheKey = externalResourcePath
+  const cached = cache.get<never>(cacheKey)
+  if (cached) return cached
+
   const proxyUrl = `${VERCEL_PROXY_BASE_URL}?url=${encodeURIComponent(`${BASE_URL}${externalResourcePath}`)}`
 
   try {
@@ -11,6 +20,7 @@ export async function fetchDataFromExternalApi(externalResourcePath: string) {
       throw new Error(`HTTP error! status: ${response.status}`)
     }
     const data = await response.json()
+    cache.set(cacheKey, data)
     return data
   } catch (error) {
     console.error('Error fetching data via proxy:', error)
